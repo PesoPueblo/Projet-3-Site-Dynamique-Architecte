527fbc185904d1bfbb4d00cf44d4163f33b84f77
Gaëtan LEPRINCE <leprince.gaetan@yahoo.fr>
2023-02-24 17:45:31 +0100

    mise en forme du code indentation rajout de la fonction "ajoutimage"

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,32 +1,38 @@
 const submit=document.querySelector('#submit');
 
 //ecouter l'envoie du form
-submit.addEventListener('click', (event) =>{
+submit.addEventListener('click', (event) => {
                   
-    event.preventDefault();
+   event.preventDefault();
 
-    //localiser les valeurs des différents input
-    const email = document.querySelector('#email').value;
-    const password= document.querySelector('#password').value;
+   //localiser les valeurs des différents input
+   const email = document.querySelector('#email').value
+   const password= document.querySelector('#password').value
 
-    //créer un objet user avec les paramètre à étudier
-    let user = {
-    email: email,
-    password: password,};
-    console.log(user);
+   //créer un objet user avec les paramètre à étudier
+   let user = {
+      email: email,
+      password: password,}
+   console.log(user);
 
-    //Faire appel à l'API pour récupérer le token
-    fetch('http://localhost:5678/api/users/login',{
-                                                  method: 'POST',
-                                                  headers: {'Content-Type': 'application/json;charset=utf-8'},
-                                                  body: JSON.stringify (user)})
-    .then(result=>result.json())
+   //Faire appel à l'API pour récupérer le token
+   fetch('http://localhost:5678/api/users/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json;charset=utf-8'},
+      body: JSON.stringify (user)
+   })
+   .then(result=>result.json())
 
-    //créer une condition pour soit redirigé soit mettre un message d'erreur 
-    .then(result =>{ console.log(result);
-                   if (result.token){
-                      localStorage.setItem("token", result.token);
-                      window.location.assign("index.html")} 
-                   else{alert("Erreur dans l’identifiant ou le mot de passe")} })
-    .catch((err)=>{console.log(err)})
-  });
+   //créer une condition pour soit redirigé soit mettre un message d'erreur 
+   .then(result => {
+      console.log(result);
+      if (result.token){
+         localStorage.setItem("token", result.token);
+         window.location.assign("index.html")
+      } 
+      else {
+         alert("Erreur dans l’identifiant ou le mot de passe")
+      }
+   })
+   .catch((err)=>{console.log('erreur authentification')})
+})
--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,50 +1,47 @@
 //récupération du token
 let token = localStorage.getItem("token");
-console.log(token);
 
 //gestion de la connexion si token existant
 if (token != null){
-
     //changement du lien login en logout et gestion du comportement
-        const linkConnect = document.querySelector("#connect");
-        linkConnect.innerHTML='<a href="index.html">logout</a>';
-        linkConnect.addEventListener('click', ()=>{localStorage.removeItem("token")});
+    const linkConnect = document.querySelector("#connect");
+    linkConnect.innerHTML='<a href="index.html">logout</a>';
+    linkConnect.addEventListener('click', ()=>{localStorage.removeItem("token")});
 
     //rajout de la bar d'admin
-        const toolAdmin = document.querySelector("#Admintoolbarhide");
-        toolAdmin.id= "Admintoolbar" ;  
+    const toolAdmin = document.querySelector("#Admintoolbarhide");
+    toolAdmin.id= "Admintoolbar"  
 
     //affichage des boutons "modifier"
-        const buttonModifier = document.querySelectorAll(".buttonmodifier");
-        buttonModifier.forEach(element => {element.removeAttribute("style")});
+    const buttonModifier = document.querySelectorAll(".buttonmodifier");
+    buttonModifier.forEach(element => {element.removeAttribute("style")});
     //cacher les filtres 
-        const filters = document.querySelector("#filtres");
-        filters.setAttribute("style", "display:none");
-    
-    }
+    const filters = document.querySelector("#filtres");
+    filters.setAttribute("style", "display:none");
+
+}
 
 //création de la fonction génération travaux pour la modal
-function generateWorkModal(works)
-{
-    for (let i=0;i< works.length;i++)
-    {
-        let work = works[i];
-        let workElementModal = document.createElement ("figure");
-        let imageElementModal = document.createElement('img');
-        imageElementModal.setAttribute ("src", work.imageUrl);
-        imageElementModal.setAttribute ("crossOrigin" , "anonymous");
-        imageElementModal.setAttribute ("alt" , work.title);
-        let buttonsuprimmer = document.createElement('i');
-        buttonsuprimmer.setAttribute('class',"fa-solid fa-trash-can")
-        let titleEditionModal = document.createElement('figcaption');
-        titleEditionModal.innerText ="éditer";
-        const divGalleryModal= document.querySelector(".gallerymodal");
-        divGalleryModal.appendChild(workElementModal);
-        workElementModal.appendChild(imageElementModal);
-        workElementModal.appendChild(buttonsuprimmer)
-        workElementModal.appendChild(titleEditionModal);
-    }
-};
+function generateWorkModal(works) {
+        for (let i=0;i< works.length;i++) {
+            let work = works[i]
+            let workElementModal = document.createElement ("figure")
+            let imageElementModal = document.createElement('img');
+            imageElementModal.setAttribute ("src", work.imageUrl);
+            imageElementModal.setAttribute ("crossOrigin" , "anonymous");
+            imageElementModal.setAttribute ("alt" , work.title);
+            let buttonsuprimmer = document.createElement('i');
+            buttonsuprimmer.setAttribute('class',"fa-solid fa-trash-can")
+            buttonsuprimmer.setAttribute('id',work.id)
+            let titleEditionModal = document.createElement('figcaption');
+            titleEditionModal.innerText ="éditer";
+            const divGalleryModal= document.querySelector(".gallerymodal");
+            divGalleryModal.appendChild(workElementModal);
+            workElementModal.appendChild(imageElementModal);
+            workElementModal.appendChild(buttonsuprimmer)
+            workElementModal.appendChild(titleEditionModal);
+        }
+}
 
 //déclaration générale
 const divGallery = document.querySelector(".gallery");
@@ -54,139 +51,178 @@ const filtreAppartements = document.getElementById("filtre_appartements");
 const filtreAutres = document.getElementById("filtre_autres");
 
 //déclare la fonction qui génère les travaux
-function generateWork(works)
-{
-    for (let i=0;i< works.length;i++)
-    {
-        let work = works[i];
+function generateWork(works) {
+    for (let i=0;i< works.length;i++) {
+        let work = works[i]
         let workElement = document.createElement ("figure");
         let imageElement = document.createElement('img');
         imageElement.setAttribute ("src", work.imageUrl);
         imageElement.setAttribute ("crossOrigin" , "anonymous");
         imageElement.setAttribute ("alt" , work.title);
         let titleElement = document.createElement('figcaption');
-        titleElement.innerText =work.title;
+        titleElement.innerText =work.title
         divGallery.appendChild(workElement);
         workElement.appendChild(imageElement);
         workElement.appendChild(titleElement);
     }
-
-};
+}
 
 // Importation des travaux via l'API 
 const travaux=fetch('http://localhost:5678/api/works')
     .then(allWorks => allWorks.json())
-    .catch(function(err){
-        console.log("Problème importation des travaux depuis l'API")});
+    .catch(function(err) {
+        console.log("Problème importation des travaux depuis l'API")
+    });
 
 //genere les travaux
 travaux .then(allWorks =>generateWork(allWorks))
-        .catch(function(err){console.log("Erreur dans la génération de tous les travaux")});
+        .catch(function(err) {
+            console.log("Erreur dans la génération de tous les travaux")
+        });
 
 
 //pour affichage Tous seulement
-filtreTous.addEventListener("click", function(){
-                                                divGallery.innerHTML="";
-                                                travaux.then(allWorks =>generateWork(allWorks))
-                                                .catch(function(err){console.log("Erreur dans la génération de tous les travaux")});
-                                                //changement de style des boutons filtre
-                                                filtreTous.classList.add("active");
-                                                filtreObjets.classList.remove("active");
-                                                filtreAppartements.classList.remove("active");
-                                                filtreAutres.classList.remove("active"); 
-                                                });
+filtreTous.addEventListener("click", function() {
+    divGallery.innerHTML="";
+    //générer les travaux 
+    travaux.then(allWorks =>generateWork(allWorks))
+    .catch(function(err){
+        console.log("Erreur dans la génération de tous les travaux")
+    });
+
+    //changement de style des boutons filtre
+    filtreTous.classList.add("active");
+    filtreObjets.classList.remove("active");
+    filtreAppartements.classList.remove("active");
+    filtreAutres.classList.remove("active"); 
+});
+
 //pour affichage Objets seulement
-filtreObjets.addEventListener("click", function(){
-                                                divGallery.innerHTML="";
+filtreObjets.addEventListener("click", function() {
+    divGallery.innerHTML="";
 
-                                                //tri des travaux en fonction de la catégorie choisi
-                                                const worksObjets = travaux
-                                                    .then(allWorks=> allWorks.filter(
-                                                        function(work){return work.categoryId == 1 }));
+    //tri des travaux en fonction de la catégorie choisi
+    const worksObjets = travaux.then(allWorks=> 
+        allWorks.filter(function(work) {
+            return work.categoryId == 1 
+        })
+    );
 
-                                                //générer les travaux en fonction de la liste obtenue 
-                                                worksObjets.then(worksObjets=>generateWork(worksObjets))
-                                                           .catch(function(err){console.log("Erreur dans la génération des travaux objets")});
+    //générer les travaux en fonction de la liste obtenue 
+    worksObjets.then(worksObjets=>generateWork(worksObjets))
+               .catch(function(err) {
+                console.log("Erreur dans la génération des travaux objets")
+               });
 
-                                                //changement de style des boutons filtre
-                                                filtreObjets.classList.add("active");
-                                                filtreTous.classList.remove("active");
-                                                filtreAppartements.classList.remove("active");
-                                                filtreAutres.classList.remove("active"); 
-                                                });
+    //changement de style des boutons filtre
+    filtreObjets.classList.add("active");
+    filtreTous.classList.remove("active");
+    filtreAppartements.classList.remove("active");
+    filtreAutres.classList.remove("active"); 
+});
 
 //pour affichage Appartements seulement
-filtreAppartements.addEventListener("click", function(){
-                                                        divGallery.innerHTML="";
+filtreAppartements.addEventListener("click", function() {
+    divGallery.innerHTML="";
 
-                                                        //tri des travaux en fonction de la catégorie choisi
-                                                        const worksAppartements = travaux
-                                                            .then(allWorks=> allWorks.filter(
-                                                                function(work){return work.categoryId == 2}));
+    //tri des travaux en fonction de la catégorie choisi
+    const worksAppartements = travaux.then(allWorks=>
+        allWorks.filter(function(work) {
+            return work.categoryId == 2
+        })
+    );
 
-                                                        //générer les travaux en fonction de la liste obtenue 
-                                                        worksAppartements.then(worksAppartements=>generateWork(worksAppartements))
-                                                                         .catch(function(err){console.log("Erreur dans la génération des travaux Appartements")});
-                                                        
-                                                        //changement de style des boutons filtre
-                                                        filtreAppartements.classList.add("active");
-                                                        filtreTous.classList.remove("active");
-                                                        filtreObjets.classList.remove("active");
-                                                        filtreAutres.classList.remove("active"); 
-                                                        });
+    //générer les travaux en fonction de la liste obtenue
+    worksAppartements.then(worksAppartements=>generateWork(worksAppartements))
+    .catch(function(err){
+        console.log("Erreur dans la génération des travaux Appartements")
+    });
+
+    //changement de style des boutons filtre
+    filtreAppartements.classList.add("active");
+    filtreTous.classList.remove("active");
+    filtreObjets.classList.remove("active");
+    filtreAutres.classList.remove("active"); 
+});
+
 //pour affichage Autres seulement
 filtreAutres.addEventListener("click", function(){
-                                                divGallery.innerHTML="";
+    divGallery.innerHTML="";
 
-                                                //tri des travaux en fonction de la catégorie choisi
-                                                const worksAutres = travaux
-                                                        .then(allWorks=> allWorks.filter(
-                                                            function(work){return work.categoryId == 3}));
+    //tri des travaux en fonction de la catégorie choisi
+    const worksAutres = travaux.then(allWorks=> 
+        allWorks.filter(function(work) {
+            return work.categoryId == 3
+        })
+    );
 
-                                                //générer les travaux en fonction de la liste obtenue 
-                                                worksAutres.then(worksAutres=>generateWork(worksAutres))
-                                                            .catch(function(err){console.log("Erreur dans la génération des travaux Autres")});
+    //générer les travaux en fonction de la liste obtenue 
+    worksAutres.then(worksAutres=>generateWork(worksAutres))
+    .catch(function(err) {
+        console.log("Erreur dans la génération des travaux Autres")
+    });
                                                 
-                                                //changement de style des boutons filtre
-                                                filtreAutres.classList.add("active");
-                                                filtreTous.classList.remove("active");
-                                                filtreAppartements.classList.remove("active");
-                                                filtreObjets.classList.remove("active");
-                                                });
+    //changement de style des boutons filtre
+    filtreAutres.classList.add("active");
+    filtreTous.classList.remove("active");
+    filtreAppartements.classList.remove("active");
+    filtreObjets.classList.remove("active");
+});
+
 //générer les travaux dans la modal
 travaux .then(allWorks =>generateWorkModal(allWorks));    
 
 //ouverture et fermeture de la boite modale
 let modal= null;
 const stopPropag = function(event){ event.stopPropagation()}
+
 const openModal= function(event) { 
     event.preventDefault();
+
+    //cible la modal à ouvrir
     const target = document.querySelector(event.target.getAttribute('href'));
-    console.log(target)
+
+    //change les paramètre pour l'afficher
     target.setAttribute('style', 'display: null');
     target.setAttribute('aria-modal', 'true');
     target.removeAttribute('aria-hidden');
     modal=target;
+
+    //gestion de la fermeture de la modal
     modal.addEventListener('click', closeModal);
+
+    let wrappeur = modal.querySelector('.modalwrapeur');
+    wrappeur.addEventListener('click', stopPropag) 
 
     let boutonClose = modal.querySelector('.close'); 
     boutonClose.addEventListener('click',closeModal);
 
-    let wrappeur = modal.querySelector('.modalwrapeur');
-    wrappeur.addEventListener('click', stopPropag)    
+     //ecoute du click poubelle
+    document.querySelectorAll(".fa-trash-can")
+    .forEach(element => {
+        element.addEventListener('click',function() {
+            deleteWorks(element)
+            })
+        }
+    );
 
-    const ajoutphoto= modal.querySelector('#ajout')
+    //event du bouton ajout photo
+    const ajoutphoto= modal.querySelector('#ajout');
     if (ajoutphoto != null){
-    ajoutphoto.addEventListener('click',closeModal)
-    ajoutphoto.addEventListener('click',openModal)}
+        ajoutphoto.addEventListener('click',closeModal);
+        ajoutphoto.addEventListener('click',openModal);
+    }
 
-    let boutonReturn = modal.querySelector('.return')
+    //event du click flèche retour 
+    const boutonReturn = modal.querySelector('.return');
     if (boutonReturn != null){
-    boutonReturn.addEventListener('click',closeModal)
-    boutonReturn.addEventListener('click',openModal)}
-    
-}
+        boutonReturn.addEventListener('click',closeModal);
+        boutonReturn.addEventListener('click',openModal);
+    }
 
+     
+}
+//fonction qui gère la fermeture des modals
 const closeModal= function(event) { 
     if (modal == null) return
     event.preventDefault();
@@ -197,11 +233,44 @@ const closeModal= function(event) {
     modal.querySelector('.close').removeEventListener('click',closeModal)
     modal.querySelector('.modalwrapeur').removeEventListener('click', stopPropag)
     modal=null
-};
+}
 
-
+//ecoute des clicks liens ouverture de modals
 document.querySelectorAll('.js-modal')
     .forEach(Element=>{
              Element.addEventListener("click",openModal);
-            })
+            });
 
+//fonction de suppression de travaux
+function deleteWorks (work) {
+    console.log(work.id);
+    if (confirm('Voulez-vous vraiement supprimé ce tarvail ?')) {
+        fetch(`http://localhost:5678/api/works/${work.id}`, {
+            method:"DELETE",
+            headers:{'Authorization': 'Bearer ' + token}
+        })
+    }
+}
+
+//ajout d'image 
+
+let form = document.querySelector('#ajoutphoto')
+form.addEventListener('submit', (ajout)=> {
+    ajout.preventDefault();
+    let title=document.querySelector('#title').value
+    console.log(title)
+    let image=document.querySelector('#newfile').files[0]
+    console.log(image)
+    let categories = document.querySelector('#categories')
+    let category = categories.value;
+    console.log(category)
+    let newWork = new FormData();
+    newWork.append('image',image);
+    newWork.append('title',title);
+    newWork.append('category',category);
+    fetch('http://localhost:5678/api/works', {
+        method: 'POST',
+        headers: {'Authorization': 'Bearer ' + token},
+        body: newWork
+    })
+})
